245fa4c977b9ddb40fa5d670f7d50a78a895e8f7
shakursmullick <shakursmullick@gmail.com>
2021-12-22 08:20:32 +0600

    Modify installer

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 sudo docker-compose -f docker-compose.yml up --build --detach
-sudo docker exec ldb_web_1 celery -A data_app.config worker --loglevel=INFO -Q enrich,db
-# sudo docker exec -d ldb_web_1 celery -A data_app.config worker --loglevel=INFO -Q enrich,db
+sudo docker exec -d ldb_web_1 celery -A data_app.config worker --loglevel=INFO -Q enrich,db
+# sudo docker exec ldb_web_1 celery -A data_app.config worker --loglevel=INFO -Q enrich,db
